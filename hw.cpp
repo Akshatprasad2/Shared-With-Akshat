532a3f0a523d7289962060d1fb60272a65e4fe21
Akshatprasad2 <93446664+Akshatprasad2@users.noreply.github.com>
2022-01-13 23:43:44 +0530

    lets see

---
+++ hw.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    float a,b,c,d,r;
+    a=2;
+    b=4;
+    c=16 ;
+    d== b*b + 4*a*c ;
+    r== d/a - 3*(b/a);
+    printf("d = %d\n",d);
+    printf("r = %d\n",r);
+        
+
+    return 0;
+}
