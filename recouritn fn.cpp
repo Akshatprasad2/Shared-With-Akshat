f1dcc8df408e71f0bc5c496a095cd50558703b95
Akshatprasad2 <93446664+Akshatprasad2@users.noreply.github.com>
2022-01-15 12:05:36 +0530

    recusive function example

---
+++ recouritn fn.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d",&x);
+    int facto(int x);
+    printf("%d",facto(x));
+    
+}
+
+int facto( int x){
+
+    int temp;
+    if (x==0){
+        return 1;
+    }
+    else{
+        temp*= x*facto(x-1);
+        return temp;
+    }
+}
